ba47a24f08708989b59cd108b3f43b870fe1331b
FCBleomessi10 <710823459@qq.com>
2023-03-14 09:36:07 +0800

    实现DB.Exec

---
+++ database/db.go
@@ -0,0 +1,90 @@
+package database
+
+import (
+	"go-redis/datastrcut/dict"
+	"go-redis/interface/database"
+	"go-redis/interface/resp"
+	"go-redis/resp/reply"
+	"strings"
+)
+
+type DB struct {
+	index  int
+	data   dict.Dict
+	addAof func(CmdLine)
+}
+
+type ExecFunc func(db *DB, args [][]byte) resp.Reply
+
+type CmdLine = [][]byte
+
+func makeDB() *DB {
+	return &DB{data: dict.MakeSyncDict(), addAof: func(line CmdLine) {}}
+}
+
+func (db *DB) Exec(c resp.Connection, cmdLine CmdLine) resp.Reply {
+	// PING SET SETNX 指令
+	cmdName := strings.ToLower(string(cmdLine[0]))
+	cmd, ok := cmdTable[cmdName]
+	if !ok {
+		return reply.MakeErrReply("ERR unknown command " + cmdName)
+	}
+	if !validateArity(cmd.arity, cmdLine) {
+		return reply.MakeArgNumErrReply(cmdName)
+	}
+
+	executor := cmd.executor
+	return executor(db, cmdLine[1:])
+}
+
+// SET K V -> arity = 3
+// EXISTS k1 k2 k3 ... -> arity = -2 最小值的负数
+func validateArity(arity int, cmdArgs [][]byte) bool {
+	argNum := len(cmdArgs)
+	if arity >= 0 {
+		return argNum == arity
+	} else {
+		return argNum >= -arity
+	}
+}
+
+func (db *DB) GetEntity(key string) (*database.DataEntity, bool) {
+	raw, ok := db.data.Get(key)
+	if !ok {
+		return nil, false
+	}
+	entity, _ := raw.(*database.DataEntity)
+	return entity, true
+}
+
+func (db *DB) PutEntity(key string, entity *database.DataEntity) int {
+	return db.data.Put(key, entity)
+}
+
+func (db *DB) PutIfExists(key string, entity *database.DataEntity) int {
+	return db.data.PutIfExists(key, entity)
+}
+
+func (db *DB) PutIfAbsent(key string, entity *database.DataEntity) int {
+	return db.data.PutIfAbsent(key, entity)
+}
+
+func (db *DB) Remove(key string) {
+	db.data.Remove(key)
+}
+
+func (db *DB) Removes(keys ...string) (deleted int) {
+	deleted = 0
+	for _, key := range keys {
+		_, exists := db.data.Get(key)
+		if exists {
+			db.data.Remove(key)
+			deleted++
+		}
+	}
+	return deleted
+}
+
+func (db *DB) Flush() {
+	db.data.Clear()
+}
