d9b9aac7b8b20f986c17ed5d955c39d336ff6457
FCBleomessi10 <710823459@qq.com>
2023-03-14 09:37:19 +0800

    实现核心DataBase

---
+++ database/standalone_database.go
@@ -0,0 +1,85 @@
+package database
+
+import (
+	"go-redis/aof"
+	"go-redis/config"
+	"go-redis/interface/resp"
+	"go-redis/lib/logger"
+	"go-redis/resp/reply"
+	"strconv"
+	"strings"
+)
+
+type StandaloneDatabase struct {
+	dbSet      []*DB
+	aofHandler *aof.AofHandler
+}
+
+func NewStandaloneDatabase() *StandaloneDatabase {
+	database := &StandaloneDatabase{}
+	if config.Properties.Databases == 0 {
+		config.Properties.Databases = 16
+	}
+
+	database.dbSet = make([]*DB, config.Properties.Databases)
+	for i := range database.dbSet {
+		db := makeDB()
+		db.index = i
+		database.dbSet[i] = db
+	}
+
+	if config.Properties.AppendOnly {
+		aofHandler, err := aof.NewAofHandler(database)
+		if err != nil {
+			panic(err)
+		}
+		database.aofHandler = aofHandler
+		for _, db := range database.dbSet {
+			sdb := db
+			sdb.addAof = func(line CmdLine) {
+				database.aofHandler.AddAof(sdb.index, line)
+			}
+		}
+	}
+	return database
+}
+
+func (database *StandaloneDatabase) Exec(client resp.Connection, args [][]byte) resp.Reply {
+	defer func() {
+		if err := recover(); err != nil {
+			logger.Error(err)
+		}
+	}()
+
+	cmdName := strings.ToLower(string(args[0]))
+	if cmdName == "select" {
+		if len(args) != 2 {
+			return reply.MakeArgNumErrReply("select")
+		}
+		return execSelect(client, database, args[1:])
+	}
+
+	dbIndex := client.GetDBIndex()
+	db := database.dbSet[dbIndex]
+	return db.Exec(client, args)
+}
+
+func (database *StandaloneDatabase) Close() {
+}
+
+func (database *StandaloneDatabase) AfterClientClose(c resp.Connection) {
+}
+
+func execSelect(c resp.Connection, database *StandaloneDatabase, args [][]byte) resp.Reply {
+	dbIndex, err := strconv.Atoi(string(args[0]))
+	if err != nil {
+		return reply.MakeErrReply("ERR invalid DB index")
+	}
+
+	if dbIndex >= len(database.dbSet) {
+		return reply.MakeErrReply("ERR DB index is out of range")
+	}
+
+	c.SelectDB(dbIndex)
+	return reply.MakeOkReply()
+}
