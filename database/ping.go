daf4575a2d2d6500989010d8ba78de9a4ddb2317
FCBleomessi10 <710823459@qq.com>
2023-03-14 09:36:27 +0800

    实现PING命令

---
+++ database/ping.go
@@ -0,0 +1,14 @@
+package database
+
+import (
+	"go-redis/interface/resp"
+	"go-redis/resp/reply"
+)
+
+func Ping(db *DB, args [][]byte) resp.Reply {
+	return reply.MakePongReply()
+}
+
+func init() {
+	RegisterCommand("ping", Ping, 1)
+}
