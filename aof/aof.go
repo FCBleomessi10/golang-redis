8052baf1c448553046809c07b4d0365295d238d8
FCBleomessi10 <710823459@qq.com>
2023-03-14 09:37:56 +0800

    实现Aof落盘和恢复

---
+++ aof/aof.go
@@ -0,0 +1,116 @@
+package aof
+
+import (
+	"go-redis/config"
+	"go-redis/interface/database"
+	"go-redis/lib/logger"
+	"go-redis/lib/utils"
+	"go-redis/resp/connection"
+	"go-redis/resp/parser"
+	"go-redis/resp/reply"
+	"io"
+	"os"
+	"strconv"
+)
+
+type CmdLine = [][]byte
+
+const aofBufferSize = 1 << 16
+
+type payload struct {
+	cmdLine CmdLine
+	dbIndex int
+}
+
+type AofHandler struct {
+	database    database.DataBase
+	aofChan     chan *payload
+	aofFile     *os.File
+	aofFilename string
+	currentDB   int
+}
+
+func NewAofHandler(database database.DataBase) (*AofHandler, error) {
+	handler := &AofHandler{}
+	handler.aofFilename = config.Properties.AppendFilename
+	handler.database = database
+
+	handler.LoadAof()
+
+	aofile, err := os.OpenFile(handler.aofFilename, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0600)
+	if err != nil {
+		return nil, err
+	}
+
+	handler.aofFile = aofile
+	handler.aofChan = make(chan *payload, aofBufferSize)
+	go func() {
+		handler.handlerAof()
+	}()
+	return handler, nil
+}
+
+func (handler *AofHandler) AddAof(dbIndex int, cmd CmdLine) {
+	if config.Properties.AppendOnly && handler.aofChan != nil {
+		handler.aofChan <- &payload{cmdLine: cmd, dbIndex: dbIndex}
+	}
+}
+
+func (handler *AofHandler) handlerAof() {
+	handler.currentDB = 0
+	for p := range handler.aofChan {
+		if p.dbIndex != handler.currentDB {
+			data := reply.MakeMultiBulkReply(utils.ToCmdLine("select", strconv.Itoa(p.dbIndex))).ToBytes()
+			_, err := handler.aofFile.Write(data)
+			if err != nil {
+				logger.Error(err)
+				continue
+			}
+			handler.currentDB = p.dbIndex
+		}
+		data := reply.MakeMultiBulkReply(p.cmdLine).ToBytes()
+		_, err := handler.aofFile.Write(data)
+		if err != nil {
+			logger.Error(err)
+			continue
+		}
+	}
+}
+
+func (handler *AofHandler) LoadAof() {
+	file, err := os.Open(handler.aofFilename)
+	if err != nil {
+		logger.Error(err)
+		return
+	}
+	defer func() {
+		_ = file.Close()
+	}()
+
+	ch := parser.ParseStream(file)
+	fackConn := &connection.Connection{}
+	for p := range ch {
+		if p.Err != nil {
+			if p.Err == io.EOF {
+				break
+			}
+			logger.Error(p.Err)
+			continue
+		}
+
+		if p.Data == nil {
+			logger.Error("empty payload")
+			continue
+		}
+		r, ok := p.Data.(*reply.MultiBulkReply)
+		if !ok {
+			logger.Error("exec err")
+			continue
+		}
+
+		rep := handler.database.Exec(fackConn, r.Args)
+		if reply.IsErrReply(rep) {
+			logger.Error(rep)
+		}
+	}
+}
