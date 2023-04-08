12917d7d4f63ac2376f0499ee0f8d96411a71771
FCBleomessi10 <710823459@qq.com>
2023-04-09 00:46:04 +0800

    完成跳表元素的插入与根据rank返回节点，测试通过

--- datastrcut/dict/sortedset/skiplist.go
+++ datastrcut/dict/sortedset/skiplist.go
@@ -1,5 +1,10 @@
 package sortedset
 
+import (
+	"math/bits"
+	"math/rand"
+)
+
 const (
 	MaxLevel = 16
 )
@@ -39,4 +44,99 @@ func makeSkipList() *skipList {
 		header: makeNode("", 0, MaxLevel),
 		level:  1,
 	}
+}
+
+func randomLevel() int16 {
+	total := uint64(1)<<uint64(MaxLevel) - 1
+	u := rand.Uint64() % total
+	return MaxLevel - int16(bits.Len64(u+1)) + 1
+}
+
+func (skiplist *skipList) insert(ele string, score float64) *skiplistNode {
+	// 1. 定位到插入节点的前一个节点
+	prev := make([]*skiplistNode, MaxLevel)
+	spans := make([]int64, MaxLevel)
+	cur := skiplist.header
+	// 1.1 从level[]的末尾遍历
+	for i := skiplist.level - 1; i >= 0; i-- {
+		if i == skiplist.level-1 {
+			spans[i] = 0
+		} else {
+			spans[i] = spans[i+1]
+		}
+		if cur.level[i] != nil {
+			for cur.level[i].forward != nil &&
+				(cur.level[i].forward.score < score ||
+					cur.level[i].forward.score == score && cur.level[i].forward.ele < ele) {
+				spans[i] += cur.level[i].span
+				cur = cur.level[i].forward
+			}
+		}
+		prev[i] = cur
+	}
+
+	// 2. 判断层数是否要扩容
+	level := randomLevel()
+	if level > skiplist.level {
+		for i := skiplist.level; i < level; i++ {
+			prev[i] = skiplist.header
+			prev[i].level[i].span = skiplist.length
+			spans[i] = 0
+		}
+		skiplist.level = level
+	}
+
+	// 3. 插入节点
+	node := makeNode(ele, score, level)
+	for i := int16(0); i < level; i++ {
+		// 3.1 更新插入节点及其前驱节点的forward
+		node.level[i].forward = prev[i].level[i].forward
+		prev[i].level[i].forward = node
+		// 3.2 更新插入节点及其前驱节点的spans
+		node.level[i].span = spans[i] + prev[i].level[i].span - spans[0]
+		prev[i].level[i].span = spans[0] - spans[i] + 1
+	}
+
+	// 4. 比插入节点的层级更高的前一个节点的跨度+1
+	for i := level; i < skiplist.level; i++ {
+		prev[i].level[i].span++
+	}
+
+	// 5. 如果插入节点的前驱节点不是头节点，那么其前驱节点不为空
+	if prev[0] == skiplist.header {
+		node.backward = nil
+	} else {
+		node.backward = prev[0]
+	}
+
+	// 6. 维护插入节点的后继节点, 将其的前驱节点指向插入节点
+	if node.level[0].forward != nil {
+		node.level[0].forward.backward = node
+	} else {
+		skiplist.tail = node
+	}
+
+	skiplist.length++
+	return node
+}
+
+// getByRank 根据排名返回节点
+func (skiplist *skipList) getByRank(rank int64) *skiplistNode {
+	var i int64 = 0
+	cur := skiplist.header
+	for level := skiplist.level - 1; level >= 0; level-- {
+		for cur.level[level].forward != nil && (i+cur.level[level].span) <= rank {
+			i += cur.level[level].span
+			cur = cur.level[level].forward
+		}
+		if rank == i {
+			return cur
+		}
+	}
+	return nil
+}
+
+func (skiplist *skipList) getRank(ele string, score float64) int64 {
+	// TODO
+	return 0
 }
--- datastrcut/dict/sortedset/skiplist_test.go
+++ datastrcut/dict/sortedset/skiplist_test.go
@@ -1,6 +1,7 @@
 package sortedset
 
 import (
+	"strconv"
 	"testing"
 )
 
@@ -8,4 +9,41 @@ func TestMakeSkipList(t *testing.T) {
 	s := *makeSkipList()
 	t.Logf("header: %v, tail: %v, length: %v, level: %v",
 		s.header, s.tail, s.length, s.level)
+}
+
+func TestInsert(t *testing.T) {
+	s := *makeSkipList()
+	for i := 0; i < 20; i++ {
+		s.insert(strconv.Itoa(i), float64(i))
+	}
+	for i, node := -1, s.header; node != nil; i, node = i+1, node.level[0].forward {
+		if e, _ := strconv.Atoi(node.ele); node != s.header && e != i {
+			t.Errorf("Wrong! insert error!")
+		}
+		for _, l := range node.level {
+			if node != s.header && l.forward != nil {
+				if l.span != int64(l.forward.score-node.score) {
+					t.Errorf("Wrong! span error!")
+				}
+			}
+		}
+		//t.Logf("ele: %v, score: %v, level: %v", node.ele, node.score, len(node.level))
+	}
+	t.Logf("level: %v, length: %v", s.level, s.length)
+}
+
+func TestGetByRank(t *testing.T) {
+	length := 1000
+	s := *makeSkipList()
+	for i := 0; i < length; i++ {
+		s.insert(strconv.Itoa(i), float64(i))
+	}
+
+	for i := 1; i <= length; i++ {
+		node := s.getByRank(int64(i))
+		if e, _ := strconv.Atoi(node.ele); node != s.header && e != i-1 {
+			t.Errorf("Wrong")
+		}
+		//t.Logf("rank: %v, ele: %v, score: %v", i, node.ele, node.score)
+	}
 }
